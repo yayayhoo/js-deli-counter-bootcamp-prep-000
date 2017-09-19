b81fde6d0866c09ceea9e2b580a810332060a904
yesI <1317154549@qq.com>
2017-09-19 02:10:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,17 @@ function takeANumber(katzDeli, name){
   katzDeli.push(name);
   var i = katzDeli.indexOf(name) + 1;
   var senten = "Welcome, "+name+". You are number "+i+" in line.";
-
   return senten;
+}
+
+function nowServing(array) {
+  var messa;
+  if (array[0] == undefined) {
+    messa = "There is nobody waiting to be served!";
+  }
+  else {
+    messa = "Currently serving ." + array[0] +'.';
+    array.shift();
+  }
+  return messa;
 }
